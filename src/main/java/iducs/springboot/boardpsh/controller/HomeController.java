478ebc78cf58b2ec74cec6b83f4c199012b65faf
kyungtae43 <201912065@office.induk.ac.kr>
2023-04-20 14:26:42 +0900

    에러 및 경로 수정

--- src/main/java/iducs/springboot/boardpsh/controller/HomeController.java
+++ src/main/java/iducs/springboot/boardpsh/controller/HomeController.java
@@ -10,24 +10,18 @@ import java.util.stream.IntStream;
 
 @Controller
 public class HomeController {
-    @Autowired
-    MemoRepository memoRepository;
 
     @GetMapping("/")
     public String goHome() {
-        IntStream.rangeClosed(1, 10).forEach(i -> {
-            MemoEntity memo = MemoEntity.builder().memoText("egyou : " + i).build();
-            memoRepository.save(memo);
-        });
-        return "/admin/index";
+        return "admin/index";
     }
 
     @GetMapping("/tables")
-    public String getTables() { return "/admin/tables"; }
+    public String getTables() { return "admin/tables"; }
 
     @GetMapping("/buttons")
-    public String getButtons() { return "/admin/buttons"; }
+    public String getButtons() { return "admin/buttons"; }
 
     @GetMapping("/cards")
-    public String getCards() { return "/admin/cards"; }
+    public String getCards() { return "admin/cards"; }
 }
